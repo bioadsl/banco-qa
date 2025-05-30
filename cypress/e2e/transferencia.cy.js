a8cbcf67918dff6effb40b31f9785c2c8c791189
fabricio <fabricio.4135@gmail.com>
2025-05-29 22:57:08 -0300

    ajuste no arquivo tranferencia.cy.js

--- cypress/e2e/transferencia.cy.js
+++ cypress/e2e/transferencia.cy.js
@@ -4,19 +4,19 @@ describe('Banco QA - Transferência', () => {
   });
 
   it('realiza transferência com saldo suficiente', () => {
-    cy.get('select').select('Carlos');
+    cy.get('select#remetente').select('Fabricio');
+    cy.get('select#destino').select('Carlos');
     cy.get('input[type="number"]').clear().type('50');
-    cy.get('input[type="date"]').clear().type('2025-05-29');
+    cy.get('input[type="date"]').clear().type('2025-05-30');
     cy.contains('Transferir').click();
     cy.get('.alert-success').should('contain', 'Transferência simulada com sucesso.');
-    cy.get('tr').contains('Fabricio').parent().should('contain', 'R$ 150,00');
-    cy.get('tr').contains('Carlos').parent().should('contain', 'R$ 250,00');
   });
 
   it('impede transferência com saldo insuficiente', () => {
-    cy.get('select').select('Joana');
-    cy.get('input[type="number"]').clear().type('300');
-    cy.get('input[type="date"]').clear().type('2025-05-29');
+    cy.get('select#remetente').select('Carlos');
+    cy.get('select#destino').select('Joana');
+    cy.get('input[type="number"]').clear().type('9999');
+    cy.get('input[type="date"]').clear().type('2025-05-30');
     cy.contains('Transferir').click();
     cy.get('.alert-danger').should('contain', 'Saldo insuficiente.');
   });
